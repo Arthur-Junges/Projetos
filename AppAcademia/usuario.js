e6c01502c77d91227e71fcc56b8d926699892c61
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-01-30 12:45:53 -0300

    .

--- AppAcademia/usuario.js
+++ AppAcademia/usuario.js
@@ -3,7 +3,10 @@ if (!email) {
     window.location.href = "index.html"
 }
 import {getUsuarios} from './script.js'
-
+const texto = document.getElementById('texto')
 const usuario = getUsuarios
 console.log(usuario)
 
+carregarUsuario();{
+    texto.innerText = `Seja bem vindo(a) ${usuario.name}`
+}
