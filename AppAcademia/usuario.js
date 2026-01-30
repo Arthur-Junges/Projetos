d7284375366d39ad8f843801d469ae012714d388
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-01-30 16:35:55 -0300

    .

--- AppAcademia/usuario.js
+++ AppAcademia/usuario.js
@@ -18,3 +18,20 @@ function carregarUsuario() {
     texto.innerText = `Seja bem vindo(a) ${usuario.nome}`
 }
 carregarUsuario()
+
+const textodois = document.getElementById('textodois')
+const botao = document.getElementById('botao')
+botao.addEventListener('click', () => {
+
+    let peso = Number(document.getElementById('peso').value)
+    let altura = Number(document.getElementById('altura').value)
+    let imc = peso / (altura * altura)
+    
+    if(peso > 0 && altura > 0){
+        textodois.innerText = imc
+    } else {
+        textodois.innerText = 'Digite igual gnt débil'
+    }
+
+})
+
